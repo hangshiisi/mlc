13490dabebdfa81dcfc28501d96c7fd3a1593d9a
Hang Shi <hangshiisi@gmail.com>
2016-12-18 21:58:11 -0800

    passed one vs all

--- san/ex3/oneVsAll.m
+++ san/ex3/oneVsAll.m
@@ -7,9 +7,9 @@ function [all_theta] = oneVsAll(X, y, num_labels, lambda)
 %   in a matrix all_theta, where the i-th row of all_theta corresponds 
 %   to the classifier for label i
 
-% Some useful variables
+% Some useful variables: m is number of samples, n is number of variables
 m = size(X, 1);
-n = size(X, 2);
+n = size(X, 2); 
 
 % You need to return the following variables correctly 
 all_theta = zeros(num_labels, n + 1);
@@ -48,14 +48,18 @@ X = [ones(m, 1) X];
 %         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
 %                 initial_theta, options);
 %
+for c = 1:num_labels
+
+  initial_theta = zeros(n + 1, 1);
+
+  options = optimset('GradObj', 'on', 'MaxIter', 50);
 
 
-
-
-
-
-
-
+  [theta] = ...
+        fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
+                 initial_theta, options);
+  all_theta(c, :) = transpose(theta)
+endfor
 
 
 
